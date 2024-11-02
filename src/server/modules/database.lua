3d71f24e0fb51a99a27d318a87248cff627397b6
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-11-02 19:36:05 +0100

    fix: removed not used function.

--- src/server/modules/database.lua
+++ src/server/modules/database.lua
@@ -1,25 +1,4 @@
 MySQL.ready(function()
     Core.DataBase.connected = true
+    print("info", "[MySQL] Connected to the database.")
 end)
-
-function Core.DataBase:AwaitReady()
-    local promise = promise.new()
-
-    if Core.DataBase.connected then
-        promise:resolve()
-    else
-        CreateThread(function()
-            local timeout = os.time() + (Config.DataBaseTimeout and type(Config.DataBaseTimeout) == "number" or 10)
-            while not Core.DataBase.connected do
-                if os.time() > timeout then
-                    promise:reject("Timeout waiting for database connection")
-                    return
-                end
-                Wait(1)
-            end
-            promise:resolve()
-        end)
-    end
-
-    return promise
-end

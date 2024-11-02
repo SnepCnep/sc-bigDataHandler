9fc448517dc5b5e482b33137b96c6f8de5d2e4ae
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-11-02 19:22:58 +0100

    fix: Timeout on database await.

--- config.lua
+++ config.lua
@@ -1,1 +1,3 @@
 Config = {}
+
+Config.DataBaseTimeout = 5
--- src/server/modules/database.lua
+++ src/server/modules/database.lua
@@ -3,7 +3,23 @@ MySQL.ready(function()
 end)
 
 function Core.DataBase:AwaitReady()
-    while not Core.DataBase.connected do
-        Citizen.Wait(0)
+    local promise = promise.new()
+
+    if Core.DataBase.connected then
+        promise:resolve()
+    else
+        CreateThread(function()
+            local timeout = os.time() + (Config.DataBaseTimeout and type(Config.DataBaseTimeout) == "number" or 10)
+            while not Core.DataBase.connected do
+                if os.time() > timeout then
+                    promise:reject("Timeout waiting for database connection")
+                    return
+                end
+                Wait(1)
+            end
+            promise:resolve()
+        end)
     end
+
+    return promise
 end

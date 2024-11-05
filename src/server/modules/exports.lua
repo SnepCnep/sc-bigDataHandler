f1f4d39a2e35143315f1b3b1b2cfe6c3b1b2a6e3
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-11-05 10:26:44 +0100

    fix: add some exports.

---
+++ src/server/modules/exports.lua
@@ -0,0 +1,10 @@
+exports("TryCatch", Core.Functions.TryCatch)
+exports("exports", Core.Functions.exports)
+exports("Init", function()
+    local init = {}
+
+    init.Functions = Core.Functions
+    init.Http = Core.http
+
+    return init
+end)

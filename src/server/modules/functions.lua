e360c86c9f04c0306a25a39b780d8dccde09242d
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-11-05 10:09:40 +0100

    fix: add export function.

--- src/server/modules/functions.lua
+++ src/server/modules/functions.lua
@@ -16,14 +16,30 @@ function Core.Functions:TryCatch(func, catch)
     return promise
 end
 
+-- [Function: exports] --
+local _exports = exports
+function Core.Functions:exports(exportName, exportFunc, exportResource)
+    if not exportName or not exportFunc then return end
+
+    if not exportResource then
+        _exports(('_cfx_export_sc-boilerplate_%s'):format(exportName), exportFunc)
+        return
+    end
+
+    _exports(('_cfx_export_%s_%s'):format(exportResource, exportName), exportFunc)
+end
+exports = function(...)
+    return Core.Functions:exports(...)
+end
+
 -- [Function: print] --
-local oldPrint = print
+local _print = print
 function Core.Functions:print(type, ...)
     local types = { ["warn"] = "^3[Core] ^3[Warn] ^7", ["error"] = "^3[Core] ^1[Error] ^7", ["info"] = "^3[Core] ^2[Info] ^7" }
     if types[type] then
-        oldPrint(types[type], ...)
+        _print(types[type], ...)
     else
-        oldPrint("^3[Core] ^7", type, ...)
+        _print("^3[Core] ^7", type, ...)
     end
 end
 print = function(...)
@@ -34,7 +50,7 @@ end
 function Core.Functions:debug(...)
     if not Config.Debug then return end
 
-    oldPrint("^3[Core]", "^7[Debug] ^4: ", ...)
+    _print("^3[Core]", "^7[Debug] ^4: ", ...)
 end
 debug = function(...)
     Core.Functions:debug(...)

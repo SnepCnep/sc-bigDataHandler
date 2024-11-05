72a7e45dc8b243b49c0e4ba68d7403f57ed1c7c4
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2024-11-05 11:41:02 +0100

    fix: some whitespaces.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,15 +10,15 @@ version "1.0.0"
 
 client_scripts { 
     "src/client/main.lua",
-    "src/client/modules/*.lua" 
+    "src/client/modules/*.lua"
 }
 server_scripts { 
-    "config.lua", 
+    "config.lua",
     "src/server/main.lua",
     "@oxmysql/lib/MySQL.lua",
     "src/server/modules/*.lua",
 }
 shared_scripts { 
-    "@es_extended/imports.lua", 
-    "@ox_lib/init.lua" 
+    "@es_extended/imports.lua",
+    "@ox_lib/init.lua"
 }
